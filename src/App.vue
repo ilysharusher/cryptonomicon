25eade7dfff0f84d109bcc4ca3a25d1e59781704
ilysharusher <illyaetoilysha@gmail.com>
2023-04-30 20:04:58 +0300

    Criptonomicon 2 is done

--- src/App.vue
+++ src/App.vue
@@ -70,6 +70,10 @@
                     <div
                         v-for="t in tickers"
                         :key="t.name"
+                        @click="select(t)"
+                        :class="{
+                            'border-4': sel === t
+                        }"
                         class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
                     >
                         <div class="px-4 py-5 sm:p-6 text-center">
@@ -80,7 +84,7 @@
                         </div>
                         <div class="w-full border-t border-gray-200"></div>
                         <button
-                            @click="handleDelete(t)"
+                            @click.stop="handleDelete(t)"
                             class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
                         >
                             <svg
@@ -101,38 +105,42 @@
                     </div>
                 </dl>
                 <hr class="w-full border-t border-gray-600 my-4" />
-                <!--            <section class="relative">
-                                <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">VUE - USD</h3>
-                                <div class="flex items-end border-gray-600 border-b border-l h-64">
-                                    <div class="bg-purple-800 border w-10 h-24"></div>
-                                    <div class="bg-purple-800 border w-10 h-32"></div>
-                                    <div class="bg-purple-800 border w-10 h-48"></div>
-                                    <div class="bg-purple-800 border w-10 h-16"></div>
-                                </div>
-                                <button type="button" class="absolute top-0 right-0">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        xmlns:xlink="http://www.w3.org/1999/xlink"
-                                        xmlns:svgjs="http://svgjs.com/svgjs"
-                                        version="1.1"
-                                        width="30"
-                                        height="30"
-                                        x="0"
-                                        y="0"
-                                        viewBox="0 0 511.76 511.76"
-                                        style="enable-background: new 0 0 512 512"
-                                        xml:space="preserve"
-                                    >
-                                        <g>
-                                            <path
-                                                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                                                fill="#718096"
-                                                data-original="#000000"
-                                            ></path>
-                                        </g>
-                                    </svg>
-                                </button>
-                            </section>-->
+                <section v-if="sel" class="relative">
+                    <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+                        {{ sel.name }} - USD
+                    </h3>
+                    <div class="flex items-end border-gray-600 border-b border-l h-64">
+                        <div
+                            v-for="(bar, id) in normalizeGraph()"
+                            :key="id"
+                            :style="{ height: `${bar}%` }"
+                            class="bg-purple-800 border w-10"
+                        ></div>
+                    </div>
+                    <button @click="sel = null" type="button" class="absolute top-0 right-0">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                            xmlns:svgjs="http://svgjs.com/svgjs"
+                            version="1.1"
+                            width="30"
+                            height="30"
+                            x="0"
+                            y="0"
+                            viewBox="0 0 511.76 511.76"
+                            style="enable-background: new 0 0 512 512"
+                            xml:space="preserve"
+                        >
+                            <g>
+                                <path
+                                    d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                                    fill="#718096"
+                                    data-original="#000000"
+                                ></path>
+                            </g>
+                        </svg>
+                    </button>
+                </section>
             </template>
         </div>
     </div>
@@ -142,23 +150,58 @@
 export default {
     data() {
         return {
-            tickers: [
-                { name: 'UAH', price: 8 },
-                { name: 'RUB', price: 150 },
-                { name: 'ZL', price: 16 }
-            ]
+            ticker: null,
+            tickers: [],
+            sel: null,
+            graph: []
         };
     },
 
     methods: {
         add() {
-            this.tickers.push({ name: this.ticker, price: '-' });
+            const newTicker = { name: this.ticker, price: 'wait' };
+
+            this.tickers.push(newTicker);
+
+            setInterval(async () => {
+                const req = await fetch(
+                    `https://min-api.cryptocompare.com/data/price?fsym=${newTicker.name}&tsyms=USD&api_key=3574a1ea559b10639c916230210b98d53d046aec99e2cea3362a3bbb137f8df8`
+                );
+                const data = await req.json();
+
+                this.tickers.find((t) => t.name === newTicker.name).price =
+                    data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+                /*this.newTicker.price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);*/
+
+                if (this.sel?.name === newTicker.name) {
+                    this.graph.push(data.USD);
+                }
+            }, 3000);
 
             this.ticker = null;
         },
 
+        normalizeGraph() {
+            const max = Math.max(...this.graph);
+            const min = Math.min(...this.graph);
+
+            return this.graph.map((g) => 5 + ((g - min) * 95) / (max - min));
+        },
+
+        select(ticker) {
+            this.sel
+                ? this.sel.name === ticker.name
+                    ? (this.sel = null)
+                    : (this.sel = ticker)
+                : (this.sel = ticker);
+            this.graph = [];
+        },
+
         handleDelete(toRemove) {
             this.tickers = this.tickers.filter((t) => t !== toRemove);
+
+            this.sel = null;
         }
     }
 };
