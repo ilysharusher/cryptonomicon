8727f6a26570e1c8434d1321a217f7e5be610be2
ilysharusher <illyaetoilysha@gmail.com>
2023-05-16 23:24:19 +0300

    Refactoring after websockets

--- src/api.js
+++ src/api.js
@@ -1,8 +1,20 @@
 const API_KEY = '3574a1ea559b10639c916230210b98d53d046aec99e2cea3362a3bbb137f8df8';
 
 const tickersHandlers = new Map();
+const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`);
 
-const loadTickers = () => {
+socket.addEventListener('message', (e) => {
+    const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(e.data);
+
+    if (type !== '5' || newPrice === undefined) {
+        return;
+    }
+
+    const handlers = tickersHandlers.get(currency) ?? [];
+    handlers.forEach((fn) => fn(newPrice));
+});
+
+/*const loadTickers = () => {
     // debugger;
     if (tickersHandlers.size === 0) return;
 
@@ -22,17 +34,54 @@ const loadTickers = () => {
                 handlers.forEach((fn) => fn(newPrice));
             });
         });
-};
+};*/
+
+function sendToWebSocket(message) {
+    const stringifiedMessage = JSON.stringify(message);
+
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(stringifiedMessage);
+        return;
+    }
+
+    socket.addEventListener(
+        'open',
+        () => {
+            socket.send(stringifiedMessage);
+        },
+        { once: true }
+    );
+}
+
+function subscribeToTickerOnWs(ticker) {
+    sendToWebSocket(
+        {
+            action: 'SubAdd',
+            subs: [`5~CCCAGG~${ticker}~USD`]
+        },
+    );
+}
+
+function unsubscribeFromTickerOnWs(ticker) {
+    sendToWebSocket(
+        {
+            action: 'SubRemove',
+            subs: [`5~CCCAGG~${ticker}~USD`]
+        },
+    );
+}
 
 export const subscribeToTicker = (ticker, cb) => {
     const subscribers = tickersHandlers.get(ticker) || [];
     tickersHandlers.set(ticker, [...subscribers, cb]);
+    subscribeToTickerOnWs(ticker);
 };
 
 export const unsubscribeFromTicker = (ticker) => {
     tickersHandlers.delete(ticker);
+    unsubscribeFromTickerOnWs(ticker);
 };
 
-setInterval(loadTickers, 3000);
+// setInterval(loadTickers, 3000);
 
 window.tickers = tickersHandlers;
